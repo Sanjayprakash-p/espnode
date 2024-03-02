c1f2f56c058bc1359c2f35d55845bd78341d1d87
Sanjay Prakash <sanjayprakashp.eee2021@citchennai.net>
2024-03-02 12:01:33 +0530

    u

--- app.js
+++ app.js
@@ -91,7 +91,7 @@
 // });
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors= require('cos')
+const cors= require('cors')
 const app = express();
 const PORT = 3000;
 app.use(cors())
